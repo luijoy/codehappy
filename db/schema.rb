d36ab41325203e3a2e2ccb85e595c2a55744bb63
Lui Kohl <lui.kohl@gmail.com>
2018-07-25 16:43:49 +0200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_25_054245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "workplaces", force: :cascade do |t|
     t.integer "CH"
     t.integer "pay"
